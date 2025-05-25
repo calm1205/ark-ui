0e46e3ac7189aad8ce7f3d6bd6ecadbe00b65c6c
calm1205 <nagi.moriyama@mntsq.com>
2025-05-25 14:19:50 +0900

    feat: set accordion

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 // import Slider from "./components/Slider.vue"
 import Checkbox from "./components/Checkbox.vue"
+import Accodion from "./components/Accodion.vue"
 </script>
 
 <template>
   <Checkbox label="check" />
+  <Accodion />
 </template>
 
 <style scoped></style>
---
+++ src/components/Accodion.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { Popover } from "@ark-ui/vue/popover"
+
+const open = ref(false)
+</script>
+
+<template>
+  <Popover.Root v-model:open="open">
+    <Popover.Trigger
+      class="flex items-center gap-2 cursor-pointer bg-gray-200 p-2"
+    >
+      Click Me
+      <Popover.Indicator>{{ open ? "v" : ">" }}</Popover.Indicator>
+    </Popover.Trigger>
+    <Popover.Positioner>
+      <Popover.Content>
+        <Popover.Title>Title1</Popover.Title>
+        <Popover.Description>Description</Popover.Description>
+      </Popover.Content>
+      <Popover.Content>
+        <Popover.Title>Title2</Popover.Title>
+        <Popover.Description>Description</Popover.Description>
+      </Popover.Content>
+      <Popover.Content>
+        <Popover.Title>Title3</Popover.Title>
+        <Popover.Description>Description</Popover.Description>
+      </Popover.Content>
+    </Popover.Positioner>
+  </Popover.Root>
+</template>
