fc6a8686c7e44bfcb652ce36f0c01b0801e1e7c9
calm1205 <nagi.moriyama@mntsq.com>
2025-05-25 12:58:29 +0900

    feat: rm un needed code

---
+++ src/components/Slider.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Slider } from "@ark-ui/vue/slider"
+</script>
+
+<template>
+  <Slider.Root>
+    <Slider.Label>Label</Slider.Label>
+    <Slider.ValueText />
+    <Slider.Control>
+      <Slider.Track>
+        <Slider.Range />
+      </Slider.Track>
+      <Slider.Thumb :index="0">
+        <Slider.HiddenInput />
+      </Slider.Thumb>
+    </Slider.Control>
+  </Slider.Root>
+</template>
+
+<style scoped>
+/* Targets the <Slider.Root /> */
+[data-scope="slider"][data-part="root"] {
+  display: flex;
+  flex-direction: column;
+}
+</style>
